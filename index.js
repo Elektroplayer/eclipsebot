7ff96de58fe1408174091d97e79a384273bf7a6a
Vitaly <38861673+Elektroplayer@users.noreply.github.com>
2021-06-13 18:00:33 +0300

    Очищен index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose  = require('mongoose');
 const CONFIG    = require('./config.json');
 const Client    = require('./lib/client.js'); //  С большой буквы потому что это класс
 
-//  Константы и переменные
+//  Создаём клиента
 const bot = new Client({
     ws: {
         intents: discord.Intents.ALL,
@@ -30,39 +30,3 @@ mongoose.connect(CONFIG.mongoToken, {useNewUrlParser: true, useUnifiedTopology: 
 
 //  Производим загрузку команд и ивентов
 bot.loadAll();
-
-//  А вот далее идёт то, что нужно расфасовать по разным файлам!
-
-/*
-bot.on('raw', async (event) => {try {EVENTS.raw(bot,event)}catch(err){console.log(err)}}) //  Да, тут теперь только один подключенный файл)
-
-bot.on('guildDelete', (guild) => {try {
-    SETTINGS.findOneAndDelete({serverID:guild.id}, (err) => {
-        if(err) console.log(err)
-        //  Заходим в БД и удаляем этот сервер.
-        //  Понимаю, бот не всегда работает и, возможно, какие-то сервера останутся, но их будет гораздо меньше.
-        //  Когда будет мало места в БД, тогда и займусь удалением остатков
-    })
-} catch(err){console.log(err)}})
-
-bot.on('guildDelete', (guild) => {
-	bot.channels.cache.get(CONFIG.feedbackChannel).send(
-		new discord.MessageEmbed().setTitle('Удалён сервер!').setColor(CONFIG.colors.errorRed)
-		.addField('Имя:', guild.name)
-		.addField('ID:', guild.id)
-		.addField('Количество людей:', guild.members.cache.size)
-	)
-});
-
-bot.on('guildMemberAdd', (member) => {try {
-    EVENTS.guildMemberAdd_server(bot,member);
-    //EVENTS.guildMemberAdd_direct(bot,member);
-    //EVENTS.guildMemberAdd_autorole(bot,member);
-} catch (err) {console.log(err)}});
-
-bot.on('guildMemberRemove', (member) => {try {
-    EVENTS.guildMemberRemove_server(bot,member);
-    //EVENTS.guildMemberAdd_direct(bot,member);
-} catch (err) {console.log(err)}});
-
-*/

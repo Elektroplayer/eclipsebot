ec96c4a3f56e33368e169d871e949fd8c345c94e
Elektroplayer <vitaly.hawanski@yandex.ru>
2021-06-20 22:04:00 +0300

    heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const discord  = require('discord.js');
 const mongoose  = require('mongoose');
 
 //  Подключение файлов
-const CONFIG    = require('./config.json');
+//const CONFIG    = require('./config.json');
 const Client    = require('./lib/client.js'); //  С большой буквы потому что это класс
 
 //  Создаём клиента
@@ -23,8 +23,8 @@ const bot = new Client({
     listenersDir: "events",
 });
 
-bot.login(CONFIG.token); //  Логиним бота
-mongoose.connect(CONFIG.mongoToken, {useNewUrlParser: true, useUnifiedTopology: true}); //  Логиним mongoose
+bot.login(process.env.TOKEN); //  Логиним бота
+mongoose.connect(process.env.MONGOTOKEN, {useNewUrlParser: true, useUnifiedTopology: true}); //  Логиним mongoose
 //  CONFIG я конечно же не дам.
 //  Подробнее о нём читай на GitHub в README.md, в разделе MiniWiki.
 

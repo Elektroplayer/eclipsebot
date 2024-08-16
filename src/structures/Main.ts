1db07b68ac6fb80d9f3eff925f6365a6e2748e84
Electroplayer <vitaly.hawanski@yandex.ru>
2024-08-16 13:17:08 +0300

    Изменена последовательность запуска

--- src/structures/Main.ts
+++ src/structures/Main.ts
@@ -4,14 +4,14 @@ import Module from './Module.js';
 
 export default class Main {
     constructor() {
-        Cache.client.login();
-
-        this.initModules()
+        this.initModules().then(() => {
+            Cache.client.login();
+        });
     }
 
     async initModules() {
-        for(let dirName of fs.readdirSync('./dist/modules/')) {
-            console.log(`[loader] [modules] [+] ${dirName}`)
+        for (let dirName of fs.readdirSync('./dist/modules/')) {
+            console.log(`[loader] [modules] [+] ${dirName}`);
             let module: Module = new (await import(`../modules/${dirName}/index.js`)).default();
 
             module.init();
